0856393129ac6710bfe58b5b695d1bc168fc6ea9
ArezoHJ <hazarajafariarezoo@gmail.com>
2026-01-14 01:16:16 +0100

    Add Google site verification

---
+++ gatsby-ssr.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      key="google-site-verification"
+      name="google-site-verification"
+      content="OZx4F5TmW47sGaerQ_WCYu-Ar5qeke4NOGnkHwq3p_4"
+    />,
+  ]);
+};
